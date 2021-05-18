1b082f913cfe43a7797cfdc519092eafaebe50bd
adamshahzad <adam.shahzad@sky.uk>
2021-05-18 16:20:18 +0100

    more spec

--- src/test/scala/flowSpec.scala
+++ src/test/scala/flowSpec.scala
@@ -1,7 +1,7 @@
-import akka.stream.impl.io.InputStreamSinkStage.Data
 import com.coding.exersises.{Fragment, Main, SenderService}
-import org.mockito.Mock
+import org.mockito.{Mock, Mockito}
 import org.mockito.Mockito.verify
+import org.mockito.MockitoSugar.mock
 import org.scalatest.Outcome
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.FixtureAnyWordSpec
@@ -9,14 +9,20 @@ import org.scalatest.wordspec.FixtureAnyWordSpec
 class flowSpec extends FixtureAnyWordSpec with Matchers{
 
   "Consume data and send via sender Service" in { f =>
-  import f._
-    Main().run(List(0x781002))
-    verify(mockSenderService).sendFragmentInfo(Fragment(0x781002))
+    import f._
+      Main().run(List(0x781002))
+      verify(mockSenderService).sendFragmentInfo(Fragment(0x781002))
+  }
+
+  "Consume invalid data and not send via sender Service" in { f =>
+    import f._
+    Main().run(List(0))
+    verify(mockSenderService, Mockito.times(0))   .sendFragmentInfo(Fragment(0x781002))
   }
 
 
     override protected def withFixture(test:  OneArgTest): Outcome = {
-      val mockSenderService = Mock[SenderService]
+      val mockSenderService = mock[SenderService]
       super.withFixture(test.toNoArgTest(FixtureParam(mockSenderService)))
     }
   case class FixtureParam(mockSenderService: SenderService)
